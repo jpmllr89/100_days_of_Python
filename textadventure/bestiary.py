768781bd808ba9e41effe099963b51061f45b4ad
jpmllr89 <jpmllr89@gmail.com>
2018-01-02 22:13:00 -0500

    Added more enemies to the bestiary

--- textadventure/bestiary.py
+++ textadventure/bestiary.py
@@ -3,5 +3,9 @@ bestiary = {
 "Fly":Player("fly", 1, 2),
 "Pencil": Player("pencil", 2, 4),
 "Paper Ball": Player("Paper Ball", 1, 5),
-"Bully": Player("Bully", 4, 10)
+"Bully": Player("Bully", 4, 10),
+"Pencil Sharpener": Player("Pencil Sharpener", 3, 6),
+"Volley Ball": Player("Volley Ball", 2, 4),
+"Basket Ball": Player("Basket Ball", 3, 5),
+"Lunch Tray": Player("Lunch Tray", 4, 4)
 }
