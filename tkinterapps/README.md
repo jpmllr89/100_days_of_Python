b24899b35fd37dbf77d98e1f9ff1595cba1e4d21
jpmllr89 <jpmllr89@gmail.com>
2018-01-18 19:23:41 -0500

    added readme.md

---
+++ tkinterapps/README.md
@@ -0,0 +1,11 @@
+#Read First
+
+##The scripts titles are self explanatory, i.e. calculator is a calculator :)
+
+###To activate, simply open a terminal in the directory, and enter:
+
+python3 <scriptname>.py
+
+for example:
+
+'python3 calculator.py' will open the calculator
--- tkinterapps/calculator.py
+++ tkinterapps/calculator.py
@@ -4,6 +4,26 @@ import time
 
 
 class Calculator():
+    def percent(self):
+        num =self.entry.get()
+        percent = float(num)/100
+        self.entry.delete(0,END)
+        self.entry.insert(END,percent)
+
+    def sqroot(self):
+        num = self.entry.get()
+        sqroot = math.sqrt(float(num))
+        self.entry.delete(0, END)
+        self.entry.insert(END, sqroot)
+
+    def power(self):
+        num = self.entry.get()
+        power = float(num)**2
+        self.entry.delete(0, END)
+        self.entry.insert(END, power)
+
+    def insert_to_entry(self, arg):
+        self.entry.insert(END, arg)
 
     def clear(self):
         box = self.entry.get()
@@ -29,28 +49,16 @@ class Calculator():
             self.entry.insert(END,answer)
 
 
-    def startNumbers(self, numpad):
-        nums = [9,8,7,6,5,4,3,2,1,0]
-        numbers = []
-        for i in range(0,10):
-          numbers.append(Button(numpad, width = 3, overrelief = SUNKEN, text=str(i), command = lambda num = i:self.insert_to_entry(num)))
-        numbers[0].grid(row=3, column = 1)
-        numbers[1].grid(row=2, column = 0)
-        numbers[2].grid(row=2, column = 1)
-        numbers[3].grid(row=2, column = 2)
-        numbers[4].grid(row=1, column = 0)
-        numbers[5].grid(row=1, column = 1)
-        numbers[6].grid(row=1, column = 2)
-        numbers[7].grid(row=0, column = 0)
-        numbers[8].grid(row=0, column = 1)
-        numbers[9].grid(row=0, column = 2)
-        Button(numpad, text=".", command = lambda:self.insert_to_entry('.')).grid(row=3, column = 2)
+    # def startNumbers(self, numpad):
 
-    def start_operators(self, operatorpad):
+
+    def generate_operatorpad(self, app):
         # commenting below two lines because for some reason, entering them like
         # the numbers function above does not work.
         # ops = ['+', '-', '/', '*']
         # operators = []
+        operatorpad = Frame(app)
+        operatorpad.pack(side=RIGHT)
         Button(operatorpad, width=3, text = '+', command = lambda:self.insert_to_entry('+')).grid(row = 0, column = 0)
         Button(operatorpad, width=3, text = '-', command = lambda:self.insert_to_entry('-')).grid(row = 1, column = 0)
         Button(operatorpad, width=3, text = '/', command = lambda:self.insert_to_entry('/')).grid(row = 2, column = 0)
@@ -63,32 +71,26 @@ class Calculator():
         Button(operatorpad, width=3, text = '=', command = lambda:self.evaluate()).grid(row = 3, column = 0, columnspan = 3)
 
 
-    def percent(self):
-        num =self.entry.get()
-        percent = float(num)/100
-        self.entry.delete(0,END)
-        self.entry.insert(END,percent)
+    def generate_numpad(self, app):
+        numpad = Frame(app)
+        numpad.configure(bg = "#222222")
+        numpad.pack(side=LEFT)
+        nums = [9,8,7,6,5,4,3,2,1,0]
+        numbers = []
+        for i in range(0,10):
+          numbers.append(Button(numpad, fg = "#222222", width = 3, overrelief = SUNKEN, text=str(i), command = lambda num = i:self.insert_to_entry(num)))
+        numbers[0].grid(row=3, column = 1)
+        numbers[1].grid(row=2, column = 0)
+        numbers[2].grid(row=2, column = 1)
+        numbers[3].grid(row=2, column = 2)
+        numbers[4].grid(row=1, column = 0)
+        numbers[5].grid(row=1, column = 1)
+        numbers[6].grid(row=1, column = 2)
+        numbers[7].grid(row=0, column = 0)
+        numbers[8].grid(row=0, column = 1)
+        numbers[9].grid(row=0, column = 2)
+        Button(numpad, text=".", command = lambda:self.insert_to_entry('.')).grid(row=3, column = 2)
 
-    def sqroot(self):
-        num = self.entry.get()
-        sqroot = math.sqrt(float(num))
-        self.entry.delete(0, END)
-        self.entry.insert(END, sqroot)
-
-    def power(self):
-        num = self.entry.get()
-        power = float(num)**2
-        self.entry.delete(0, END)
-        self.entry.insert(END, power)
-
-    def insert_to_entry(self, arg):
-        self.entry.insert(END, arg)
-
-    def main():
-        app=Tk()
-        # app.geometry("300x300")
-        calc = Calculator(app)
-        app.mainloop()
 
     def __init__(self, app):
         app.title('Calculator')
@@ -99,10 +101,16 @@ class Calculator():
         self.entry = Entry(answer)
         self.entry.pack(fill=X, expand = True)
         # creates the number pad
-        numpad = Frame(app)
-        numpad.pack(side=LEFT)
-        self.startNumbers(numpad)
+        self.generate_numpad(app)
         # create operation pad
-        operatorpad = Frame(app)
-        operatorpad.pack(side=RIGHT)
-        self.start_operators(operatorpad)
+        self.generate_operatorpad(app)
+
+
+def main():
+    app=Tk()
+    app.configure(background = "#222222")
+    calc = Calculator(app)
+    app.mainloop()
+
+if __name__ == "__main__":
+    main()
